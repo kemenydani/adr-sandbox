9203af3c6c7ab39da5894813e93d0fd4f35f6de3
kemenydani <kemenydani93@gmail.com>
2018-08-20 15:16:33 +0200

    progress

--- App/User/Repository/UserRecord.php
+++ App/User/Repository/UserRecord.php
@@ -10,4 +10,69 @@ class UserRecord extends Record
     protected $UserName;
     protected $Email;
     protected $Password;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->Id;
+    }
+
+    /**
+     * @param mixed $Id
+     */
+    public function setId($Id): void
+    {
+        $this->Id = $Id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserName()
+    {
+        return $this->UserName;
+    }
+
+    /**
+     * @param mixed $UserName
+     */
+    public function setUserName($UserName): void
+    {
+        $this->UserName = $UserName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->Email;
+    }
+
+    /**
+     * @param mixed $Email
+     */
+    public function setEmail($Email): void
+    {
+        $this->Email = $Email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->Password;
+    }
+
+    /**
+     * @param mixed $Password
+     */
+    public function setPassword($Password): void
+    {
+        $this->Password = $Password;
+    }
+
 }
